e7b8c6b12700d48904bc0287449e402bd69d26d9
mins00oo <alstn7223@gmail.com>
2025-06-09 23:19:29 +0900

    [20250609] 프로젝트 초기 세팅
    DB연결 정보 및 개인설정은 env 파일로 분리

---
+++ src/main/java/com/app/devfolio/DevfolioServerApplication.java
@@ -0,0 +1,13 @@
+package com.app.devfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevfolioServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevfolioServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/devfolio/DevfolioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.devfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevfolioServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
